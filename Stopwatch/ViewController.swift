2f22c1013c376768e2a0281fca1414b7c4ee8a1c
Kangie Chan <aries0420@gmail.com>
2014-11-23 23:18:32 -0800

    Initial Commit

---
+++ Stopwatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stopwatch
+//
+//  Created by K C on 11/23/14.
+//  Copyright (c) 2014 K C. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
