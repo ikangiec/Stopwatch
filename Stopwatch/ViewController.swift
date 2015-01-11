318f8c54f504d263805b6d54cb782e0566d03253
Kangie Chan <aries0420@gmail.com>
2015-01-10 20:25:14 -0800

    Functional simple stopwatch

--- Stopwatch/ViewController.swift
+++ Stopwatch/ViewController.swift
@@ -10,9 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var timer = NSTimer()
+
+    @IBOutlet weak var time: UILabel!
+    var count = 0
+
+    @IBAction func play(sender: AnyObject) {
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("result"), userInfo: nil, repeats: true)
+    }
+
+    @IBAction func pause(sender: AnyObject) {
+        timer.invalidate()
+    }
+
+    @IBAction func reset(sender: AnyObject) {
+        timer.invalidate()
+        count = 0
+        time.text = "0"
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func result() {
+        count++
+        time.text = String(count)
     }
 
     override func didReceiveMemoryWarning() {
